efc10cb40a7288c576f0ee54809eadfb6f7504ee
macgrz <89933854+macgrz@users.noreply.github.com>
2021-12-13 11:29:06 +0100

    Update wine_quality_SVM.py

--- wine_quality_SVM.py
+++ wine_quality_SVM.py
@@ -1,8 +1,9 @@
 ### DATA USED: https://archive.ics.uci.edu/ml/datasets/Wine+Quality ###
 ### ML with white wine quality data set
 ### Basic pipeline with SVM classification and StandardScaler
+### Comparisson of Accuracy (scaled and unscaled)
 
-from sklearn import SVC
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
@@ -20,13 +21,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Instantiate the GridSearchCV object
 cv = GridSearchCV(pipeline, param_grid=parameters)
 
-# Fit to the training set
-cv.fit(X_train, y_train)
+# Fit to the scaled training set
+svm_scaled = cv.fit(X_train, y_train)
+
+# Instantiate and fit a SVM classifier to the unscaled data
+svm_unscaled = SVC().fit(X_train, y_train)
 
 # Predict the labels of the test set: y_pred
-y_pred = cv.predict(X_test)
+y_pred = svm_scaled.predict(X_test)
 
-# Compute and print metrics
-print("Accuracy: {}".format(cv.score(X_test, y_test)))
-print(classification_report(y_test, y_pred))
-print("Tuned Model Parameters: {}".format(cv.best_params_))
+# Compute and print metrics of scaled SVM
+print("Accuracy (scaled): {}".format(svm_scaled.score(X_test, y_test)))
+print("Tuned Model Parameters (scaled): {}".format(svm_scaled.best_params_))
+# print(classification_report(y_test, y_pred))
+
+# Compute and print metrics of unscaled SVM
+print('Accuracy (unscaled): {}'.format(svm_unscaled.score(X_test, y_test)))
